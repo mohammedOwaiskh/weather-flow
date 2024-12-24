bd4929b88a431a156c5222081b3d1a5a436c3eca
Mohammed Owais Khan <mohdowaiskh@gmail.com>
2024-12-24 16:16:58 +0530

    test 2

--- data-processor/sparkutils.py
+++ data-processor/sparkutils.py
@@ -9,3 +9,7 @@ def get_spark_session() -> SparkSession:
     conf.set("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0")
     conf.set("spark.sql.shuffle.partitions", 4)
     return SparkSession.builder.appName("WeatherFlow").config(conf=conf).getOrCreate()
+
+
+if __name__ == "__main__":
+    print("HI")
