6bec6e5558a231d1f393bf89a9b2f756d581a57a
Mohammed Owais Khan <mohdowaiskh@gmail.com>
2024-12-25 13:08:59 +0530

    fix 2 for 'No FileSystem for scheme "s3"'

--- data-processor/sparkutils.py
+++ data-processor/sparkutils.py
@@ -6,6 +6,9 @@ def get_spark_session() -> SparkSession:
     conf = SparkConf()
     conf.setMaster("local")
     conf.set("spark.streaming.stopGracefullyOnShutdown", True)
-    conf.set("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0")
+    conf.set(
+        "spark.jars.packages",
+        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0,com.amazonaws:aws-java-sdk-pom:1.10.34,org.apache.hadoop:hadoop-aws:2.7.2",
+    )
     conf.set("spark.sql.shuffle.partitions", 4)
     return SparkSession.builder.appName("WeatherFlow").config(conf=conf).getOrCreate()
